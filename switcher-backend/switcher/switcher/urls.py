f7fba4549f01e4f5f70e5a299b3497ad0837fd2a
rain.vink <rain.vink@gmail.com>
2017-03-27 00:13:25 +0300

    Added jwt signin, verification and refresh to backend

--- switcher-backend/switcher/switcher/urls.py
+++ switcher-backend/switcher/switcher/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/auth/login', obtain_jwt_token),
+    url(r'^api/auth/token/verify', verify_jwt_token),
+    url(r'^api/auth/token/refresh', refresh_jwt_token),
 ]
